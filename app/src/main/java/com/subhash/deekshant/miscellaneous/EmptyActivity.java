dc9ce3083a30c2d051b025f96e3cba18ff0957f8
deekshant1 <23013.deekshant@gmail.com>
2020-12-14 16:41:09 +0530

    Initial commit

---
+++ app/src/main/java/com/subhash/deekshant/miscellaneous/EmptyActivity.java
@@ -0,0 +1,14 @@
+package com.subhash.deekshant.miscellaneous;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.subhash.deekshant.R;
+
+public class EmptyActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+    }
+}
